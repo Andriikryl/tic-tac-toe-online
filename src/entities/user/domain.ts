06e7a546b2ee435d9add635cef94e84536aa61a5
Andriikryl <andreyk557@gmail.com>
2025-04-09 18:22:31 +0300

    fix mising type

--- src/entities/user/domain.ts
+++ src/entities/user/domain.ts
@@ -11,13 +11,15 @@ export type UserEntity = {
 export type SessionEntity = {
     id: UserId,
     login: string,
+    expiredAt: string
 }
 
 export const DEFAULT_RATING = 1000
 
-export const userToSession = (user:UserEntity):SessionEntity => {
+export const userToSession = (user:UserEntity, expiredAt: string):SessionEntity => {
     return {
         id: user.id,
-        login: user.login
+        login: user.login,
+        expiredAt
     }
 }
